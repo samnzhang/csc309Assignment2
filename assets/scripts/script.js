1341ee7c09c3dc8db93a123f042740b4f853db5a
Sam Zhang <samzhang@inside-220-243.wireless.utoronto.ca>
2016-06-05 16:54:38 -0400

    Can now load canvas on click.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,37 @@
-window.onload = function() {
-    
-    var c = document.getElementById("main");
-    window.ctx = c.getContext("2d"); // Dealing with a global context is easier
-    
-    // draw();
-    
-    // animate();
-    start();
-    
-}
+function loadCanvas() {
+	var canvas = document.createElement('canvas');
+	canvas.id = 'main';
+	canvas.width = '1000';
+	canvas.height = '640';
 
-var shapes = new Array();
-var Shape = function (x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-
-function start() {
-	window.ctx.clearRect(0, 0, 1000, 640);
-	shapes.push (new Shape(300, 200));
-
+    document.getElementById('start-page').appendChild(canvas);
 
 }
+
+// window.onload = function() {
+    
+//     var c = document.getElementById("main");
+//     window.ctx = c.getContext("2d"); // Dealing with a global context is easier
+    
+//     // draw();
+    
+//     // animate();
+//     start();
+    
+// }
+
+
+
+// var shapes = new Array();
+// var Shape = function (x, y) {
+//     this.x = x;
+//     this.y = y;
+// }
+
+
+// function start() {
+// 	window.ctx.clearRect(0, 0, 1000, 640);
+// 	shapes.push (new Shape(300, 200));
+
+
+// }
