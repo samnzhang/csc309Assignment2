6a5c68c691149e54c218048c5f473720f9c89425
Sam Zhang <samzhang@Sams-MacBook-Pro.local>
2016-06-15 22:06:24 -0400

    Final change.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -133,20 +133,25 @@ function infoBar() {
  * on click.
  */
 function clickBlackHole(event) {
+	// Getting user click location
 	var mouseX = event.clientX - c.getBoundingClientRect().left;
 	var mouseY = event.clientY - c.getBoundingClientRect().top;
 
+	// Checks if the uses clicked on a blackhole.
 	if (checkCollision(blackHoles, mouseX, mouseY, 2)) {
 		if (!pause && !over) {
 			for (var i = 0; i < blackHoles.length; i++) {
 				var blackHole = [];
 				blackHole.push(blackHoles[i]);
+				// Stops changing the status of all objects being pulled by
+				// the black hole.
 				if (checkCollision(blackHole, mouseX, mouseY, 2)) {
 					for(item in blackHoles[i].pulling) {
 						blackHoles[i].pulling[item].pull = null;
 						blackHoles[i].pulling[item].pulling = false;
 					}
 
+					// Increment the score by the type of black hole clicked.
 					if (blackHoles[i].type == 0) {
 						score += 5;
 					} else if (blackHoles[i].type == 1) {
@@ -155,44 +160,22 @@ function clickBlackHole(event) {
 						score += 20;
 					}
 
+					// Finally, remove the black hole.
 					blackHoles.splice(i, 1);
-
 				}
 			}
 		}
 	}
-	
-	console.log(spaceThings);
-
+	// checks if the pause button is clicked.
 	if (checkPause(mouseX, mouseY) && !over) {
 		pause = !pause;
-		console.log(pause);
 	}
 }
 
-
-function SpaceThing(x, y, type) {
-	this.x = x;
-	this.y = y;
-	this.direction = getDirection();
-	this.pull = null;
-	this.type = type;
-	this.pulling = false;
-}
-
-function BlackHole(x, y, type) {
-	this.x = x;
-	this.y = y;
-	this.type = type;
-	this.pulling = [];
-	this.pulledItems = 0;
-}
-
-function Direction(x, y) {
-	this.x = x;
-	this.y = y;
-}
-
+/*
+ * Checks if objects in the array things are in type range of x and y;
+ * Return true if a collision occurs, false otherwise.
+ */
 function checkCollision(things, x, y, type) {
 	var difference;
 	var xRight;
@@ -200,6 +183,7 @@ function checkCollision(things, x, y, type) {
 	var yTop;
 	var yBottom;
 
+	// Types of collision to be determined
 	if (type == 0) {
 		difference = 50;
 	} else if (type == 1) {
@@ -210,14 +194,14 @@ function checkCollision(things, x, y, type) {
 		difference = 3;
 	}
 
+	// Checks each object in things if they are colliding with the object on
+	// x and y
 	for (var i = 0; i < things.length; i++) {
 
 		xRight = things[i].x + difference;
 		xLeft = things[i].x - difference;
 		yTop = things[i].y - difference;
 		yBottom = things[i].y + difference;
-
-	
 
 		if (xRight < x) {
 			continue;
@@ -231,30 +215,33 @@ function checkCollision(things, x, y, type) {
 			return true;
 		}
 	}
+	// No objects are colliding with things.
 	return false;
 }
 
+/*
+ * Provided with the user's click location, check if the user clicked on the
+ * pause button. Return true if click, false otherwise.
+ */
 function checkPause(x, y) {
+	// Pause button location.
+	var xRight = 870;
+	var xLeft = 770;
+	var yTop = 5;
+	var yBot = 35;
 
-	var xRight;
-	var xLeft;
-	var yTop;
-	var yBottom;
-
-	xRight = 870;
-	xLeft = 770;
-	yTop = 5;
-	yBottom = 35;
-
-	if (xRight < x || xLeft > x || yTop > y || yBottom < y) {
+	// Checks is pause button is clicked.
+	if (xRight < x || xLeft > x || yTop > y || yBot < y) {
 		return false;
 	} else {
 		return true;
 	}
-	
 }
 
-
+/*
+ * Checks if object at x, y are colliding with any blackholes.
+ * Return the blackhole if collision occurs, null if no collision.
+ */
 function checkInHorizon(x, y) {
 	var difference = 100;
 	var xRight;
@@ -262,14 +249,12 @@ function checkInHorizon(x, y) {
 	var yTop;
 	var yBottom;
 
+	// Checking for all blackholes if they collide with x, y.
 	for (var i = 0; i < blackHoles.length; i++) {
-
 		xRight = blackHoles[i].x + difference;
 		xLeft = blackHoles[i].x - difference;
 		yTop = blackHoles[i].y - difference;
 		yBottom = blackHoles[i].y + difference;
-
-
 
 		if (xRight < x) {
 			continue;
@@ -283,15 +268,21 @@ function checkInHorizon(x, y) {
 			return blackHoles[i];
 		}
 	}
+	// No objects are in collision path.
 	return null;
 }
 
-
+/*
+ * Randomly generate directions for the space objects.
+ */
 function getDirection() {
 	var direction = directions[Math.floor(Math.random() * 4)];
 	return direction;
 }
 
+/*
+ * Inserting ten space objects into random places.
+ */
 function insertSpaceThings() {
 
 	for (var i = 0; i < 10; i++) {
@@ -300,10 +291,14 @@ function insertSpaceThings() {
 			var y = Math.floor(Math.random() * 535) + 65;
 		}
 		while (checkCollision(spaceThings, x, y, 0));
+
 		spaceThings.push(new SpaceThing(x, y, i));
 	}
 }
 
+/*
+ * Assigning the objects in spaceThings an "look"
+ */
 function drawSpaceThings() {
 	for (item in spaceThings) {
 		var x = spaceThings[item].x;
@@ -344,7 +339,10 @@ function drawSpaceThings() {
 	}
 }
 
-
+/*
+ * Caculates the pull directions given the blackholes location and the
+ * space object's location.
+ */
 function calculatePullDirection(x, y, targetX, targetY, t) {
 	var vector = new Direction();
 	var xdirect;
@@ -354,14 +352,25 @@ function calculatePullDirection(x, y, targetX, targetY, t) {
 	return vector;
 }
 
+/*
+ * Main function for drawing and erasing objects on the canvas.
+ */
 function moveSpaceThings() {
 	if (!pause && !over) {
-		
+		// Clear the canvas.
 		ctx.clearRect(0, 0, 1000, 640);
+		// Draw the remaining objects that are not sucked in by the 
+		// blackhole.
 		drawSpaceThings();
+		// Draw the blackholes.
 		drawBlackHoles();
+		// Draw the info bar.
 		infoBar();
+		// Checks if the game is over;
 		gameOver();
+
+		// Generate a move direction for the space objects if they are not
+		// being pulled by a black hole.
 		for (item in spaceThings) {
 			var object = spaceThings[item];
 			if (object.pull == null) {
@@ -371,51 +380,71 @@ function moveSpaceThings() {
 					(object.y + object.direction.y) >= 615) {
 					object.direction = getDirection();
 				} 
-			
+				
+				// Checks if an object is crossing the event horizon.
 				object.pull = checkInHorizon(object.x, object.y);
 				if (object.pull != null) {
 					object.pull.pulling.push(object);
 				}
+
 			} else {
 				var directionX;
 				var directiony;
 
+				// If the object is being pull by the black hole,
+				// generate the pull direction and speed for the object.
 				if (object.pulling == false) {
 					switch (object.pull.type) {
+						// Speed is slow for blue black holes.
 						case 0: 
-							object.direction = calculatePullDirection(object.x, object.y, object.pull.x, object.pull.y, 50);
+							object.direction = calculatePullDirection(
+								object.x, object.y, 
+								object.pull.x, object.pull.y, 50);
 							break;
+						// Speed is moderate for purple black holes.
 						case 1:
-							object.direction = calculatePullDirection(object.x, object.y, object.pull.x, object.pull.y, 30);
+							object.direction = calculatePullDirection(
+								object.x, object.y, 
+								object.pull.x, object.pull.y, 30);
 							break;
+						// Speed is fast for black black holes.
 						case 2:
-							object.direction = calculatePullDirection(object.x, object.y, object.pull.x, object.pull.y, 15);
+							object.direction = calculatePullDirection(
+								object.x, object.y, 
+								object.pull.x, object.pull.y, 15);
 							break;
 					}
 					object.pulling = true;
-
 				}
-			
 			}
+			// Change the object's position in the direction of the pull.
 			object.x += object.direction.x;
 			object.y += object.direction.y;
-
 		}
+		// Checks if an object is at the center of a black hole. Disappears
+		// if it is at the center.
 		eventHorizon();
 	}
+
+	// If the game is in an transition phase.
 	if (!over && timeLeft == 0) {
 		if (level == 1) {
+			// Change the directions if an object was being pulled by a black
+			// hole right before the game stopped.
 			for (item in spaceThings) {
 				spaceThings[item].pull = null;
 				spaceThings[item].pulling = false;
 			}
+			// Display the next info page.
 			levelOverview();
 			level += 1;
 		} else {
 			levelOverview();
 		}
+		// Ends this function.
 		return null;
-		
+
+	// If the game is over, shows the finish info page and ends the function.
 	} else if (over) {
 		levelOverview();
 		return null;
@@ -423,6 +452,9 @@ function moveSpaceThings() {
 	setTimeout(moveSpaceThings, 33);
 }
 
+/*
+ * Updates the highscore of the game.
+ */
 function setHighscore(score) {
 	var temp;
 	if (score > localStorage.highScore1) {
@@ -442,51 +474,75 @@ function setHighscore(score) {
 
 }
 
+/*
+ * Display and modify the highscore info on the html.
+ */
 function displayHighscore() {
 	document.getElementById("highscore1").innerHTML = localStorage.highScore1;
 	document.getElementById("highscore2").innerHTML = localStorage.highScore2;
 	document.getElementById("highscore3").innerHTML = localStorage.highScore3;
 }
 
+/*
+ * Hides the canvas and displays the next level info page with buttons.
+ */
 function levelOverview() {
+	// Modify the level and the score.
 	document.getElementById("level").innerHTML = "Level " + level;
 	document.getElementById("score").innerHTML = "Your Score: " + score;
+	// Hides canvas.
 	var canvas = document.getElementById("main");
 	canvas.style.display = 'none';
+	// Displays the next level transition page.
 	var next = document.getElementById("next");
 	next.style.display = 'block';
+	// Display the credit for the SVG images.
 	var credit = document.getElementById("credit");
 	credit.style.display = 'initial';
 	var nextLevel = document.getElementById("nextLevel");
 	var finish = document.getElementById("finish");
 
+	// Hides finish button if it is level 1 and game is not over.
 	if (level == 1 && !over) {
 		nextLevel.style.display = 'initial';
 		finish.style.display = 'none';
+	// Else, hides the next button.
 	} else {
 		nextLevel.style.display = 'none';
 		finish.style.display = 'initial';
 		setHighscore(score);
 		displayHighscore();
 	}
+	// Clear the intervals for counting time and inserting blackholes.
 	clearInterval(countTime);
 	clearInterval(insertSpeed);
 }
 
+/*
+ * Initializes level 2 with 60 secs and no blackholes.
+ */
 function initLevel() {
+	// Hides the next level transition pages and shows canvas.
 	var canvas = document.getElementById("main");
 	canvas.style.display = 'initial';
 	var next = document.getElementById("next");
 	next.style.display = 'none';
+	// Hides the credit.
 	var credit = document.getElementById("credit");
 	credit.style.display = 'none';
+	// Initializes the blackholes and time.
 	blackHoles = [];
 	timeLeft = 60;
+	// Clear the previous blackholes and time intervals.
 	clearInterval(countTime);
 	clearInterval(insertSpeed);
+	// Starts the game.
 	startGame();
 }
 
+/*
+ * Goes back to the starting page if the game ended, and finish button clicked.
+ */
 function restart() {
 	displayHighscore();
 	var next = document.getElementById("next");
@@ -495,16 +551,19 @@ function restart() {
 	start.style.display = 'block';
 }
 
+/*
+ * Insert a black hole into a random position but not on top of each other.
+ */
 function insertBlackHoles() {
 	if (!pause && !over) {
 		do {
 			var x = Math.floor(Math.random() * 950) + 25;
 			var y = Math.floor(Math.random() * 535) + 65;
-
 		} 
 		while (checkCollision(blackHoles, x, y, 1) && blackHoles.length < 20);
 
 		var type = Math.floor(Math.random() * 15);
+		// Setting the probability of getting each types of blackholes.
 		if (blackHoles.length < 20) {
 			if (type < 9) {
 				blackHoles.push(new BlackHole(x, y, 0));
@@ -516,47 +575,66 @@ function insertBlackHoles() {
 		}
 	}
 }
+
+/*
+ * Draw black holes based on their types: blue, purple, black.
+ */
 function drawBlackHoles() {
 	for (item in blackHoles) {
 		switch(blackHoles[item].type) {
 			case 0:
-				ctx.drawImage(blackHoleBlue, blackHoles[item].x-25, blackHoles[item].y-25, 50, 50);
+				ctx.drawImage(blackHoleBlue, blackHoles[item].x-25, 
+					blackHoles[item].y-25, 50, 50);
 				break;
 			case 1:
-				ctx.drawImage(blackHolePurple, blackHoles[item].x-25, blackHoles[item].y-25, 50, 50);
+				ctx.drawImage(blackHolePurple, blackHoles[item].x-25, 
+					blackHoles[item].y-25, 50, 50);
 				break;
 			case 2:
-				ctx.drawImage(blackHoleBlack, blackHoles[item].x-25, blackHoles[item].y-25, 50, 50);
+				ctx.drawImage(blackHoleBlack, blackHoles[item].x-25, 
+					blackHoles[item].y-25, 50, 50);
 				break;
 		}
 		
 	}
 }
 
+/*
+ * Checks if an objects is at the center of a black hole, remove the object 
+ * from the game if so.
+ */
 function eventHorizon() {
 	for (var i = 0; i < spaceThings.length; i++) {
-		var collision = checkCollision(blackHoles, spaceThings[i].x, spaceThings[i].y, 3);
+		// Check if any objects are at the center of the black hole
+		var collision = checkCollision(blackHoles, 
+			spaceThings[i].x, spaceThings[i].y, 3);
 
 		if (collision) {
 			for (var j = 0; j < blackHoles.length; j++) {
 				var blackHole = [];
 				blackHole.push(blackHoles[j]);
-				if (checkCollision(blackHole, spaceThings[i].x, spaceThings[i].y, 3)) {
+				if (checkCollision(blackHole, 
+					spaceThings[i].x, spaceThings[i].y, 3)) {
 					blackHoles[j].pulledItems += 1;
 
-					if (blackHoles[j].type == 0 && blackHoles[j].pulledItems == 3) {
+					// Checks if a black hole ate too much,
+					// base on its type, remove the object if it eats too much
+					if (blackHoles[j].type == 0 && 
+						blackHoles[j].pulledItems == 3) {
 						for (item in blackHoles[j].pulling) {
 							blackHoles[j].pulling[item].pull = null;
 							blackHoles[j].pulling[item].pulling = false;
 						}
 						blackHoles.splice(j, 1);
-					} else if (blackHoles[j].type == 1 && blackHoles[j].pulledItems == 2) {
+					} else if (blackHoles[j].type == 1 && 
+						blackHoles[j].pulledItems == 2) {
 						for (item in blackHoles[j].pulling) {
 							blackHoles[j].pulling[item].pull = null;
 							blackHoles[j].pulling[item].pulling = false;
 						}
 						blackHoles.splice(j, 1);
-					} else if (blackHoles[j].type == 2 && blackHoles[j].pulledItems == 1) {
+					} else if (blackHoles[j].type == 2 && 
+						blackHoles[j].pulledItems == 1) {
 						for (item in blackHoles[j].pulling) {
 							blackHoles[j].pulling[item].pull = null;
 							blackHoles[j].pulling[item].pulling = false;
@@ -565,27 +643,61 @@ function eventHorizon() {
 					}
 				}
 			}
+			// Remove the object at the center of the black hole.
 			spaceThings.splice(i, 1);
+			// Decrease the score by 50.
 			score -= 50;
 		}
 	}
 }
-
+/*
+ * Starts the game by starting the time countdown and start inserting
+ * blackholes.
+ */
 function startGame() {
-	// setInterval(infoBar, 500);
+
 	if (level == 1) {
 		insertSpaceThings();
+		// Interval for inserting blackholes in level 1.
 		window.insertSpeed = setInterval(insertBlackHoles, 1500);
 	}
-
+	// Time counter.
 	window.countTime = setInterval(countDown, 1000);
 	
 	if (level == 2) {
+		// Interval for inserting blackholes in level 2.
 		window.insertSpeed = setInterval(insertBlackHoles, 750);
 	}
+	// Starts moving things on the canvas.
 	moveSpaceThings();
-	// drawSpaceThings();
 }
+
+// Class for space object.
+function SpaceThing(x, y, type) {
+	this.x = x;
+	this.y = y;
+	this.direction = getDirection();
+	this.pull = null;
+	this.type = type;
+	this.pulling = false;
+}
+
+// Class for a black hole.
+function BlackHole(x, y, type) {
+	this.x = x;
+	this.y = y;
+	this.type = type;
+	this.pulling = [];
+	this.pulledItems = 0;
+}
+
+// Class for a direction.
+function Direction(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+// Draw functions for each various canvas object.
 
 function drawUFO(x, y) {
 	ctx.beginPath();
